afe9ebfff7869ed9f3a183f84207f67a937aa15e
Ajay Singh <51924221+tavisca-asingh@users.noreply.github.com>
2019-06-23 08:59:51 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -22,7 +22,61 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
+            /* Saperating Value and Operand form Equation */
+            string[] string1 = equation.Split("=");
+            string[] digits = string1[0].Split("*");
+            string operand_1=digits[0];
+            string operand_2=digits[1];
+            string operand_3=string1[1];
+             
+            
+            /* Condition to check if question mark "?" is present */
+            if(operand_3.Contains("?"))                      
+            {   int value_one,value_two;
+                string equation_1;
+               value_one=Convert.ToInt32(operand_1);
+               value_two=Convert.ToInt32(operand_2);
+               value_one=value_one*value_two;
+               equation_1=value_one.ToString();
+               return findValue(equation_1,operand_3);
+            }
+            else 
+            {    
+                 int value_one,value_two;
+                 string equation_1;
+                if(operand_1.Contains("?"))
+                {
+                    string temp=operand_1;
+                   operand_1=operand_2;
+                   operand_2=temp;
+                }   
+                value_one=Convert.ToInt32(operand_1);
+                value_two=Convert.ToInt32(operand_3);
+                
+                /* if second is not divisible by first the return -1*/
+                
+                if(value_two%value_one!=0) return -1;
+                 
+                 /* Calculate Value */
+                value_one=value_two/value_one;
+                equation_1=value_one.ToString();
+                return findValue(equation_1,operand_2);
+            }
+
+            throw new NotImplementedException();
+        }
+        /* Find Value */
+        public static int findValue(string a, string b)
+        {
+            if(a.Length!=b.Length) 
+               {
+                   return -1;
+               }
+            for(int i=0;i<a.Length;i++)
+            {
+                if(b[i]=='?') return a[i]-48;
+            }
+            return -1;
             throw new NotImplementedException();
         }
     }
